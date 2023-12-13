4a4973d42a0aae5fa3749aeb02062e0a4da46b2b
Branflakes333 <brandonm333@outlook.com>
2023-12-13 00:53:31 -0800

    edited compare_hist

--- credit_card_fraud.py
+++ credit_card_fraud.py
@@ -7,17 +7,17 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 
 
-def compare_hist(df, param, type):
-    type_t = df[df[type] == 1]
-    type_f = df[df[type] == 0]
+def compare_hist(df, param, types):
+    type_t = df[df[types] == 1]
+    type_f = df[df[types] == 0]
 
     fig, axes = plt.subplots(1, 3)
     ax = axes.ravel()
     ax[0].hist(df[param], bins=100)
     ax[0].set_title(param, "Total")
     ax[1].hist(type_t[param], bins=100)
-    ax[1].set_title(param, type, "=TRUE")
+    ax[1].set_title(param, types, "=TRUE")
     ax[2].hist(type_f[param], bins=100)
-    ax[2].set_title(param, type, "=FALSE")
+    ax[2].set_title(param, types, "=FALSE")
 
     plt.tight_layout()
